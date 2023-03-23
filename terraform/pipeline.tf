e07c6b2fa3c34897e80cb3bff3123f0718bf3265
Jacek Laskowski <jacek@japila.pl>
2023-03-23 19:19:16 +0100

    Terraform

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -1,4 +1,6 @@
 terraform {
+  # Each argument in the required_providers block enables one provider
+  # The key determines the provider's local name (its unique identifier within this module)
   required_providers {
     databricks = {
       source  = "databricks/databricks"
@@ -9,6 +11,11 @@ terraform {
   required_version = ">= 1.4.0"
 }
 
+# https://developer.hashicorp.com/terraform/language/providers/configuration#default-provider-configurations
+# A provider block without an alias argument is the default configuration for that provider.
+# Resources that don't set the provider meta-argument
+# will use the default provider configuration that matches the first word of the resource type name.
+# E.g. databricks_repo, databricks_pipeline below
 provider "databricks" {}
 
 resource "databricks_repo" "learn_databricks" {
