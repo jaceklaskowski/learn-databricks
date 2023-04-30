43e8fa160f3acdf64fe3503cf99f236aa970eced
jacek@japila.pl <jacek@japila.pl>
2023-04-30 20:49:24 +0000

    More PySpark
    1. Basic Aggregation with pandas UDFs
    2. Markdown sections

--- PySpark/PySpark.py
+++ PySpark/PySpark.py
@@ -1,4 +1,12 @@
 # Databricks notebook source
+# MAGIC %md # PySpark
+
+# COMMAND ----------
+
+# MAGIC %md # Create DataFrame From NumPy Array
+
+# COMMAND ----------
+
 # MAGIC %pip install numpy matplotlib scipy
 
 # COMMAND ----------
@@ -11,8 +19,33 @@ dbutils.library.restartPython()
 import numpy as np
 rng = np.random.default_rng()
 numbers = rng.normal(size=10_000)
-display(spark.createDataFrame(numbers))
+nums = spark.createDataFrame(numbers)
+display(nums)
 
 # COMMAND ----------
 
+# MAGIC %md # Basic Aggregation with pandas UDFs
 
+# COMMAND ----------
+
+import pandas as pd
+from pyspark.sql.functions import pandas_udf
+
+# COMMAND ----------
+
+@pandas_udf(returnType = "long")
+def my_count(s: pd.Series) -> 'long':
+    return pd.Series(s.count())
+
+# COMMAND ----------
+
+from pyspark.sql.functions import abs
+grouped_nums = (nums
+    .withColumn("gid", abs((nums.value * 100) % 2))
+    .groupBy("gid"))
+count_by_gid_agg = my_count("gid").alias("count")
+counts_by_gid = grouped_nums.agg(count_by_gid_agg)
+
+# COMMAND ----------
+
+display(counts_by_gid)
