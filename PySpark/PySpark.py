e3855edbee052b9b58be2cecd840e0ea3876b3e2
jacek@japila.pl <jacek@japila.pl>
2023-05-19 06:36:41 +0000

    PySpark

--- PySpark/PySpark.py
+++ PySpark/PySpark.py
@@ -24,6 +24,12 @@ display(nums)
 
 # COMMAND ----------
 
+# MAGIC %md # Standard Functions
+# MAGIC
+# MAGIC [pyspark.sql.functions](https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/functions.html)
+
+# COMMAND ----------
+
 # MAGIC %md # Basic Aggregation with pandas UDFs
 
 # COMMAND ----------
@@ -33,19 +39,49 @@ from pyspark.sql.functions import pandas_udf
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC
+# MAGIC Learn more about [pandas.Series](https://pandas.pydata.org/docs/reference/api/pandas.Series.html).
+
+# COMMAND ----------
+
+@pandas_udf(returnType = "long")
+def group_id(vs: pd.Series) -> pd.Series:
+    return (vs.abs() * 1000).round() % 2
+
+# COMMAND ----------
+
+with_gid = nums.withColumn("gid", group_id(nums.value))
+display(with_gid)
+
+# COMMAND ----------
+
+display(with_gid.groupby("gid").count())
+
+# COMMAND ----------
+
 @pandas_udf(returnType = "long")
 def my_count(s: pd.Series) -> 'long':
     return pd.Series(s.count())
 
 # COMMAND ----------
 
-from pyspark.sql.functions import abs
-grouped_nums = (nums
-    .withColumn("gid", abs((nums.value * 100) % 2))
-    .groupBy("gid"))
+grouped_nums = with_gid.groupBy("gid")
 count_by_gid_agg = my_count("gid").alias("count")
 counts_by_gid = grouped_nums.agg(count_by_gid_agg)
 
 # COMMAND ----------
 
 display(counts_by_gid)
+
+# COMMAND ----------
+
+# MAGIC %md # DataFrame Partitions
+# MAGIC
+# MAGIC [pyspark.sql.functions.spark_partition_id](https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/api/pyspark.sql.functions.spark_partition_id.html#pyspark.sql.functions.spark_partition_id)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import spark_partition_id
+
+display(counts_by_gid.withColumn("spark_partition_id", spark_partition_id()))
