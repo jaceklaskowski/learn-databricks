f1b6caa291584876bca65cdac22daf0e071e3791
Jacek Laskowski <jacek@japila.pl>
2023-07-02 11:45:51 +0000

    Awesome Photon

---
+++ Photon.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %md # Photon
+# MAGIC
+# MAGIC This notebook is a list of _things_ (articles, talks, demos, etc.) to learn Photon from.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Slogans
+# MAGIC
+# MAGIC [Slogan](https://dictionary.cambridge.org/dictionary/english/slogan) is a _a short and striking or memorable phrase used in advertising._ so it makes much sense to learn Photon by how the people behind this product want us to remember it.
+# MAGIC
+# MAGIC * [The next generation engine for the Lakehouse](https://www.databricks.com/product/photon)
+# MAGIC * [Photon: A High-Performance Query Engine for the Lakehouse](https://www.cidrdb.org/cidr2022/papers/a100-behm.pdf)
+# MAGIC * [Photon: A Fast Query Engine for Lakehouse Systems](https://cs.stanford.edu/~matei/papers/2022/sigmod_photon.pdf)
+
+# COMMAND ----------
+
+# MAGIC %md ## Learn More
+# MAGIC
+# MAGIC ### Reading
+# MAGIC
+# MAGIC 1. [Photon: A Fast Query Engine for Lakehouse Systems](https://cs.stanford.edu/~matei/papers/2022/sigmod_photon.pdf) (_Photon whitepaper_)
+# MAGIC 1. [Photon: A High-Performance Query Engine for the Lakehouse](https://www.cidrdb.org/cidr2022/papers/a100-behm.pdf)
+# MAGIC
+# MAGIC ### Watching
+# MAGIC
+# MAGIC 1. [Photon for Dummies: How Does this New Execution Engine Actually Work?](https://www.databricks.com/dataaisummit/session/photon-dummies-how-does-new-execution-engine-actually-work/)
+# MAGIC 1. [Advancing Spark - The Photon Whitepaper](https://youtu.be/hxvQxI4FksY)
+# MAGIC 1. [Photon Technical Deep Dive: How to Think Vectorized](https://youtu.be/pNn5W4ujP3w)
+
+# COMMAND ----------
+
+
