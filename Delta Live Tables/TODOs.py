f5214a5b1d8d4cc314d5160c5343d4500f4091d2
Jacek Laskowski <jacek@japila.pl>
2023-10-02 20:59:46 +0000

    Delta Live Tables

---
+++ Delta Live Tables/TODOs.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md # Open Topics / TODOs
+# MAGIC
+# MAGIC The following is a list of things to explore in more details.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC 1. CDF Demo
+# MAGIC 2. STREAMING clause
+# MAGIC 3. Continuous execution pipeline mode
