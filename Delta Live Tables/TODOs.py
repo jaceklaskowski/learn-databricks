63e43f248cc95e29775cddaa69a7251084204cf0
jaceklaskowski <jaceklaskowski>
2023-10-03 19:28:46 +0000

    Databricks Workshop Day 2

--- Delta Live Tables/TODOs.py
+++ Delta Live Tables/TODOs.py
@@ -10,3 +10,14 @@
 # MAGIC 1. CDF Demo
 # MAGIC 2. STREAMING clause
 # MAGIC 3. Continuous execution pipeline mode
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC 1. What's the diff between abfss vs adls?
+# MAGIC 1. Different uses of [libraries](https://docs.databricks.com/api/azure/workspace/pipelines/create) in a DLT pipeline (esp. `file` and `jar` JSON fields)
+
+# COMMAND ----------
+
+
