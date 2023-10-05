8fcc253941668c24554bcf93b2a21e2dbfbf7357
Jacek Laskowski <jacek@japila.pl>
2023-10-05 20:31:09 +0000

    After Databricks Workshop Day 2

--- Delta Live Tables/Agenda.sql
+++ Delta Live Tables/Agenda.sql
@@ -22,3 +22,46 @@
 -- MAGIC %md ## Open Topics / TODOs
 -- MAGIC
 -- MAGIC [Open Topics / TODOs]($./TODOs)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC
+-- MAGIC ## Topics
+-- MAGIC
+-- MAGIC * How to work with files in Databricks
+-- MAGIC * `/FileStore`
+-- MAGIC * `dbfs` magic command
+-- MAGIC * Parameters in jobs vs DLT pipelines
+-- MAGIC * How to parameterized SQL queries (to define parameters at job level)
+-- MAGIC     * https://docs.databricks.com/en/sql/user/queries/query-parameters.html
+
+-- COMMAND ----------
+
+-- MAGIC %md ## Heads-Up
+-- MAGIC
+-- MAGIC 1. You can use Python in a SQL notebook for a DLT pipeline yet it won't be rendered in a dataflow (and vice versa)
+
+-- COMMAND ----------
+
+-- MAGIC %md ## Exercise
+-- MAGIC
+-- MAGIC Based on https://jaceklaskowski.github.io/spark-workshop/exercises/spark-sql-exercise-Using-upper-Standard-Function.html
+-- MAGIC
+-- MAGIC Create a DLT pipeline that does the following:
+-- MAGIC
+-- MAGIC 1. FIXME Accepts a parameter - a CSV filename to load
+-- MAGIC 1. FIXME Accepts another parameter that is the column name with string values
+-- MAGIC 1. Executes `upper` standard function on this string column
+-- MAGIC
+-- MAGIC FIXMEs = how to pass parameters to a DLT pipeline
+-- MAGIC
+-- MAGIC In summary:
+-- MAGIC
+-- MAGIC The dataflow (pipeline) should be two tables
+
+-- COMMAND ----------
+
+-- MAGIC %md ## Exercise (Databricks SQL)
+-- MAGIC
+-- MAGIC Create a job with a DLT pipeline (that's already created) and a (SQL) query
