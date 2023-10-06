c2fceca351b73b4c79b61d06f25c55fe46fae663
jaceklaskowski <jaceklaskowski>
2023-10-06 09:20:05 +0000

    Databricks SQL

---
+++ Databricks SQL/Agenda.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md # Databricks SQL » Agenda
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC | # | Module |
+# MAGIC | --- | --- |
+# MAGIC | 0 | [Introduction]($./Databricks SQL) |
+# MAGIC | 1 | [Queries]($./Queries) |
+# MAGIC | 2 | [Dashboards]($./Dashboards) |
+# MAGIC | 3 | [Alerts]($./Alerts) |
