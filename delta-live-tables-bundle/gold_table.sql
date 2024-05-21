63f81017fe6c42914a68097ecb0c368a760de748
jacek@japila.pl <jacek@japila.pl>
2024-05-21 08:18:23 +0000

    Bundle

---
+++ delta-live-tables-bundle/gold_table.sql
@@ -0,0 +1,9 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC
+-- MAGIC [INSERT](https://docs.databricks.com/en/sql/language-manual/sql-ref-syntax-dml-insert-into.html)
+
+-- COMMAND ----------
+
+INSERT OVERWRITE students
+VALUES ('Ashua Hill', '456 Erica Ct, Cupertino', 111111)
