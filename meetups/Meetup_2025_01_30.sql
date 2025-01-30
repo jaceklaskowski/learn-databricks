d635750e45d74aed353a44bc5561121256eed5e7
jacek@japila.pl <jacek@japila.pl>
2025-01-30 15:23:35 +0000

    Pre-meetup changes
    Data Quality in Databricks Workflows with Pydantic

--- meetups/Meetup_2025_01_30.sql
+++ meetups/Meetup_2025_01_30.sql
@@ -1,21 +1,21 @@
 -- Databricks notebook source
--- MAGIC %md # Meetup 2025-01-09
+-- MAGIC %md # Data Quality in Databricks Workflows with Pydantic
 -- MAGIC
--- MAGIC ➡️ [Deploying Databricks Workflows with uv and Databricks Asset Bundles](https://www.meetup.com/warsaw-data-engineering/events/305473028/)
+-- MAGIC ➡️ [Meetup Announcement](https://www.meetup.com/warsaw-data-engineering/events/305877678/)
 -- MAGIC
 -- MAGIC Agenda:
 -- MAGIC
--- MAGIC 1. 5 minut rogrzewki na luźne pomysły na ten i przyszłe meetupy
--- MAGIC     * News (new versions, etc.)
+-- MAGIC 1. 5 minut rozgrzewki na luźne pomysły na ten i przyszłe meetupy
+-- MAGIC     * News (new versions, new features, etc.)
 -- MAGIC 1. 50 minut Live coding session, a w nim:
--- MAGIC     * Stworzenie projektu w Pythonie z uv
--- MAGIC     * Stworzenie Databricks job z notebookiem z naszym projektem w Pythonie wyżej (wszystko ręcznie / klikamy w UI / pełny manual)
+-- MAGIC     * Stworzysz nowy projekt dla libki w Pythonie z Pydantic (hello world itp.) i jedynie słusznym uv do zarządzania projektem
+-- MAGIC     * Stworzysz Databricks job z notebookiem z naszym projektem w Pythonie wyżej (wszystko ręcznie / klikamy w UI / pełny manual)
 -- MAGIC     * Automatyzacja z Databricks Asset Bundles (DAB)
--- MAGIC 1. Q&A / Zbieranie pomysłów na kolejne edycje (5 minut)
+-- MAGIC 1. 5 minut Q&A / Zbieranie pomysłów na kolejne edycje
 
 -- COMMAND ----------
 
--- MAGIC %md # News
+-- MAGIC %md # 📢 News
 
 -- COMMAND ----------
 
@@ -23,18 +23,22 @@
 -- MAGIC
 -- MAGIC ## New Versions
 -- MAGIC
--- MAGIC * [uv 0.5.16](https://github.com/astral-sh/uv/releases/tag/0.5.16)
--- MAGIC * [Databricks CLI 0.238.0](https://github.com/databricks/cli/releases/tag/v0.238.0)
--- MAGIC * [Delta Lake 3.3.0](https://github.com/delta-io/delta/releases/tag/v3.3.0)
--- MAGIC * [awscli 2.22.31](https://github.com/aws/aws-cli/releases/tag/2.22.31)
+-- MAGIC * [uv 0.5.25](https://github.com/astral-sh/uv/releases/tag/0.5.25)
+-- MAGIC * [Databricks CLI 0.240.0](https://github.com/databricks/cli/releases/tag/v0.240.0)
 
 -- COMMAND ----------
 
 -- MAGIC %md
 -- MAGIC
--- MAGIC ## Open focus mode
+-- MAGIC ## Databricks Notebook UI
 -- MAGIC
 -- MAGIC [Databricks notebook interface and controls](https://docs.databricks.com/en/notebooks/notebook-ui.html)
+-- MAGIC
+-- MAGIC **Cmd + Shift + P** for [Command palette](https://docs.databricks.com/en/notebooks/notebook-editor.html) with the following:
+-- MAGIC
+-- MAGIC 1. [Multicursor support](https://docs.databricks.com/en/notebooks/notebook-editor.html#multicursor-support) 🥳
+-- MAGIC 1. [Use web terminal and Databricks CLI](https://docs.databricks.com/en/notebooks/notebook-editor.html#use-web-terminal-and-databricks-cli) 🤔
+-- MAGIC 1. Duplicating lines as in Visual Code ❤️
 
 -- COMMAND ----------
 
--- meetups/README.md
+++ meetups/README.md
@@ -1,3 +1,7 @@
 # Meetups
 
-This directory contains the "agenda" notebooks that were used during **Databricks Talks** series of the [Warsaw Data Engineering](https://www.meetup.com/warsaw-data-engineering/) meetup group.
+This directory contains the "agenda" notebooks from the past **Databricks Talks** series of the [Warsaw Data Engineering](https://www.meetup.com/warsaw-data-engineering/) meetup group.
+
+1. [2025-01-30](./Meetup_2025_01_30.sql)
+1. [2025-01-09](./Meetup_2025_01_09.sql)
+1. [2024-12-12](./Meetup_2024_12_12.sql)
