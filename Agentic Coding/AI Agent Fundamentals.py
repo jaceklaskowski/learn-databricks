b1b6339ea08f67635f25f90a0b40908df630160e
jacek@japila.pl <jacek@japila.pl>
2026-05-05 17:24:19 +0000

    Agent Bricks and AI Agent Fundamentals

---
+++ Agentic Coding/AI Agent Fundamentals.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # AI Agent Fundamentals
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Prompting Techniques
+# MAGIC
+# MAGIC 1. **Zero-shot** prompts a model with no examples, relying on pre-trained knowledge.
+# MAGIC 1. **One-shot** provides a single example to guide format or logic.
+# MAGIC 1. [Multi- / Few-Shot](https://learnprompting.org/docs/basics/few_shot) (_Learn by Example_)
+# MAGIC 1. [Chain of Thought](https://arxiv.org/abs/2201.11903) (_Think Out Loud_)
+# MAGIC 1. [ReAct](https://www.promptingguide.ai/techniques/react) (_Reasoning and Acting_)
+# MAGIC 1. [Retrieval Augmented Generation (RAG)](https://www.promptingguide.ai/techniques/rag)
---
+++ Databricks Agent Bricks/00 Agent Bricks.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Databricks Agent Bricks
+# MAGIC
+# MAGIC [Agent Bricks](https://www.databricks.com/product/artificial-intelligence/agent-bricks)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Learn More
+# MAGIC
+# MAGIC * [Partner courses on Databricks' Agent Bricks](https://partner-academy.databricks.com/pages/91/agents-bricks-catalog-partners)
+# MAGIC * [Prompt Engineering Fundamentals](https://partner-academy.databricks.com/learn/courses/4733/prompt-engineering-fundamentals)
+# MAGIC * [Get Started with AI Agents on Databricks](https://partner-academy.databricks.com/learn/courses/4464/get-started-with-ai-agents-on-databricks)
+# MAGIC * [Brickbites for partners: AI agents & Agent Bricks](https://partner-academy.databricks.com/learn/learning-plans/452/brickbites-for-partners-ai-agents-agent-bricks)
+# MAGIC * [AI Agent Fundamentals Learning Plan](https://partner-academy.databricks.com/learn/learning-plans/449/ai-agent-fundamentals-learning-plan)
