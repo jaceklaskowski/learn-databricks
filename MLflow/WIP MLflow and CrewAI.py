5a5f7789437fc0b5bc4f24c4573e4af99888c272
jacek@japila.pl <jacek@japila.pl>
2026-06-12 17:48:22 +0000

    WIP MLflow and CrewAI

---
+++ MLflow/WIP MLflow and CrewAI.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # WIP MLflow and CrewAI
+# MAGIC
+# MAGIC [GitHub](https://github.com/crewAIInc/crewAI)
+# MAGIC
+# MAGIC [Tracing CrewAI using MLflow](https://mlflow.org/docs/latest/genai/tracing/integrations/listing/crewai/)
